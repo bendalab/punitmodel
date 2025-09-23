556258c4aa46602a1952c7e3d441519e220d0189
Jan Grewe <jan.grewe@g-node.org>
2025-09-23 11:55:51 +0200

    [model] add exponentiation after rectification, exponent defaults to 1.0

--- model.py
+++ model.py
@@ -44,7 +44,7 @@ def load_models(file):
 def simulate(stimulus, deltat=0.00005, v_zero=0.0, a_zero=2.0,
              threshold=1.0, v_base=0.0, delta_a=0.08, tau_a=0.1,
              v_offset=-10.0, mem_tau=0.015, noise_strength=0.05,
-             input_scaling=60.0, dend_tau=0.001, ref_period=0.001):
+             input_scaling=60.0, dend_tau=0.001, ref_period=0.001, power=1.0):
     """Simulate a P-unit.
 
     Parameters
@@ -64,26 +64,27 @@ def simulate(stimulus, deltat=0.00005, v_zero=0.0, a_zero=2.0,
     -------
     spike_times: 1-D array
         Simulated spike times in seconds.
-    """    
+    """
     # initial conditions:
     v_dend = stimulus[0]
     v_mem = v_zero
     adapt = a_zero
 
-    # prepare noise:    
+    # prepare noise:
     noise = np.random.randn(len(stimulus))
     noise *= noise_strength / np.sqrt(deltat)
 
     # rectify stimulus array:
     stimulus = stimulus.copy()
     stimulus[stimulus < 0.0] = 0.0
+    stimulus **= power
 
     # integrate:
     spike_times = []
-    for i in range(len(stimulus)):
-        v_dend += (-v_dend + stimulus[i]) / dend_tau * deltat
+    for i, (s, n) in enumerate(zip(stimulus, noise)):
+        v_dend += (-v_dend + s) / dend_tau * deltat
         v_mem += (v_base - v_mem + v_offset + (
-                    v_dend * input_scaling) - adapt + noise[i]) / mem_tau * deltat
+                    v_dend * input_scaling) - adapt + n) / mem_tau * deltat
         adapt += -adapt / tau_a * deltat
 
         # refractory period:
